acc0af45c569c7736b65b6c748db695cd2be8957
John Shea <sheajohn@google.com>
2023-05-26 10:51:09 -0400

    Migrate to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,13 @@
 
 buildscript {
     extra.apply {
-        set("compose_compiler_version", "1.5.3")
-        set("lifecycle_version", "2.8.7")
+        set("compose_compiler_version", "1.4.7")
+        set("lifecycle_version", "2.6.1")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
 }
