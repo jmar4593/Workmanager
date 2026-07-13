b5e303a79c8b499fa52a09eb6850c9ed66ac6a66
CodeGoblin <jmar4593@yahoo.com>
2026-07-13 01:50:06 -0400

    Finished "Background work with WorkManager" module

--- app/src/main/java/com/example/bluromatic/workers/SaveImageToFileWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/SaveImageToFileWorker.kt
@@ -1,25 +1,8 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.bluromatic.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
 import androidx.work.CoroutineWorker
@@ -28,14 +11,12 @@ import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
 import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
-import com.example.bluromatic.domain.CreateImageUriUseCase
-import com.example.bluromatic.domain.SaveImageUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
+import java.util.Date
 
 /**
  * Saves the image to a permanent file
@@ -67,25 +48,9 @@ class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : CoroutineW
                 val bitmap = BitmapFactory.decodeStream(
                     resolver.openInputStream(Uri.parse(resourceUri))
                 )
-
-                val imageUrl: String?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    val createImageUriUseCase = CreateImageUriUseCase()
-                    val saveImageUseCase = SaveImageUseCase()
-
-                    val imageUri = createImageUriUseCase(
-                        resolver,
-                        "${title}_${dateFormatter.format(Date())}.jpg"
-                    )
-
-                    saveImageUseCase(resolver, imageUri, bitmap)
-                    imageUrl = imageUri.toString()
-                } else {
-                    @Suppress("DEPRECATION")
-                    imageUrl = MediaStore.Images.Media.insertImage(
-                        resolver, bitmap, title, dateFormatter.format(Date())
-                    )
-                }
+                val imageUrl = MediaStore.Images.Media.insertImage(
+                    resolver, bitmap, title, dateFormatter.format(Date())
+                )
                 if (!imageUrl.isNullOrEmpty()) {
                     val output = workDataOf(KEY_IMAGE_URI to imageUrl)
 
@@ -93,9 +58,7 @@ class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : CoroutineW
                 } else {
                     Log.e(
                         TAG,
-                        applicationContext.resources.getString(
-                            R.string.writing_to_mediaStore_failed
-                        )
+                        applicationContext.resources.getString(R.string.writing_to_mediaStore_failed)
                     )
                     Result.failure()
                 }
